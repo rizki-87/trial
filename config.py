0b80fbdb811c31c708b4cccd4be7a2b6e11a3040
rizki-87 <140106584+rizki-87@users.noreply.github.com>
2025-01-09 21:46:03 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,7 @@
 PREDEFINED_PASSWORD = "securepassword123"  
   
 TECHNICAL_TERMS = {  
+    "Sediver", "CoV", "quantitatively", "th", "databooks", "ngagement", "ransaction", "fr", "T", "Appendices", "LATAM", "insulators", "Sediver", "Acquiror", "Nusco", "P", "LTDA", "roposed"
     "IfError", "s", "Sarl", "SEVES", "XYZG", "Galova", "galova", "fayet", "Fayet", "Sarl", "KMPG",
     "TensorFlow", "Caret", "ML", "DALL-E", "MLOps", "PyTorch", "MENA", "Keras", "Scikit-learn", "NumPy", "Pandas", "DALL",  
     "Matplotlib", "OpenAI", "GPT-3", "Deep Learning", "Neural Network", "Data Science", "Seaborn", "Jupyter",  
