7e48caa3990fa25ff7113dfcc1ae8cf6b4e03c56
rizki-87 <140106584+rizki-87@users.noreply.github.com>
2025-01-12 20:49:12 +0700

    Update million_notation_validation.py

--- utils/million_notation_validation.py
+++ utils/million_notation_validation.py
@@ -1,23 +1,23 @@
-import re  
-import logging  
+   import re  
+   import logging  
   
-def validate_million_notations_with_pandas(df, notation='m'):  
-    issues = []  
-      
-    # Tentukan pola notasi juta  
-    if notation.lower() == 'm':  
-        pattern = r'[\€\$]?\s*\d{1,3}(?:\.\d{3})*(?:,\d+)?\s?[mM]\b'  
-    elif notation.lower() == 'mn':  
-        pattern = r'[\€\$]?\s*\d{1,3}(?:\.\d{3})*(?:,\d+)?\s?Mn\b'  
-    else:  
-        pattern = r'[\€\$]?\s*\d{1,3}(?:\.\d{3})*(?:,\d+)?\s?M\b'  
+   def validate_million_notations_with_pandas(df, notation='m'):  
+       issues = []  
+         
+       # Tentukan pola notasi juta  
+       if notation.lower() == 'm':  
+           pattern = r'[\€\$]?\s*\d{1,3}(?:\.\d{3})*(?:,\d+)?\s?[mM]\b'  
+       elif notation.lower() == 'mn':  
+           pattern = r'[\€\$]?\s*\d{1,3}(?:\.\d{3})*(?:,\d+)?\s?Mn\b'  
+       else:  
+           pattern = r'[\€\$]?\s*\d{1,3}(?:\.\d{3})*(?:,\d+)?\s?M\b'  
   
-    for index, row in df.iterrows():  
-        if re.search(pattern, row['text']):  
-            issues.append({  
-                'slide': index + 1,  # Indeks slide  
-                'issue': 'Found Million Notation',  
-                'text': row['text']  
-            })  
+       for index, row in df.iterrows():  
+           if re.search(pattern, row['text']):  
+               issues.append({  
+                   'slide': index + 1,  # Indeks slide  
+                   'issue': 'Found Million Notation',  
+                   'text': row['text']  
+               })  
   
-    return issues  
+       return issues  
