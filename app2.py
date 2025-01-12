ed9a6e4b0733bb4141824e3bff669b26029dfaec
rizki-87 <140106584+rizki-87@users.noreply.github.com>
2025-01-12 20:52:29 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -17,7 +17,7 @@ from utils.font_validation import validate_fonts_slide
 from utils.grammar_validation import initialize_language_tool, validate_grammar_slide      
 from utils.spelling_validation import is_exempted, validate_spelling_slide      
 from utils.decimal_validation import validate_decimal_consistency      
-from utils.million_notation_validation import validate_million_notations_with_pandas      
+from utils.million_notation_validation_with_pandas import validate_million_notations_with_pandas      
 from utils.validation import validate_tables, validate_charts      
 from config import PREDEFINED_PASSWORD, TECHNICAL_TERMS, NUMERIC_TERMS      
   
